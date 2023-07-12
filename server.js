f47e8a1ba992e49e1729a17141fd8a83a7c0a66e
Daly <dalyna64@gmail.com>
2023-07-12 12:19:44 +0100

    first model

--- server.js
+++ server.js
@@ -1,25 +1,21 @@
+const express = require("express");
+require("./config/connect");
 
+const app = express();
 
-const express = require('express')
-const app = express()
-app.listen(3000)
-app.get('/', (req, res) => {
-    res.send('server work !')
-    console.log('server work !');
-})
-app.get('/getAll', (req, res) => {
-    res.send('get work !')
-    console.log('get work !');
-})
-app.post('/add', (req, res) => {
-    res.send('post work !')
-    console.log('post work !');
-})
-app.put('/update', (req, res) => {
-    res.send('update work !')
-    console.log('update work !');
-})
-app.delete('/delete', (req, res) => {
-    res.send('delete work !')
-    console.log('delete work !');
-})
+app.listen(3000, () => {
+  console.log("server work !");
+});
+
+app.get("/getAll", () => {
+  console.log("get work !");
+});
+app.post("/add", () => {
+  console.log("post work !");
+});
+app.put("/update", () => {
+  console.log("update work !");
+});
+app.delete("/delete", () => {
+  console.log("delete work !");
+});
